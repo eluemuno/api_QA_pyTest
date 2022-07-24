4ee744f804ddf9bf751fc27c4e7cee6191f80160
Eluem-uno <75106535+eluemuno@users.noreply.github.com>
2022-07-24 19:45:04 -0400

    Add files via upload

---
+++ tests/test_summary_headline.py
@@ -0,0 +1,32 @@
+import country_code_run
+import pytest
+import json
+import datetime
+import os
+import re
+import logging
+
+logging.basicConfig(level=logging.INFO, filename=os.path.dirname(os.path.abspath('country_code_run.py'))
+                                                 + '/logs/test_' + datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+                                                 + '.txt',
+                    format='%(process)d--%(asctime)s--%(levelname)s--%(message)s')
+
+
+# orig = json.loads(build_payload())['data']['attributes']['segments'][0]['origin']['countryCode']
+# dest = json.loads(build_payload())['data']['attributes']['segments'][0]['destination']['countryCode']
+
+
+def test_attr_segments_summary_headline():
+    data_segment_summary_headline = country_code_run.read_json_file()['data']['attributes']['segments'][0]['summary'][
+        'headline']
+    reSpecialCharacter = re.compile('[@_!#$%^&*<>?/}{~:]')
+    check_for_special_character = reSpecialCharacter.search(data_segment_summary_headline)
+    assert check_for_special_character is None
+
+
+def test_attr_summary_headlines():
+    data_attr_summary_headlines = country_code_run.read_json_file()['data']['attributes']['summary']
+    for i in range(len(data_attr_summary_headlines)):
+        reSpecialCharacter = re.compile('[@_!#$%^&*<>?|}{~:]')
+        check_for_special_character = reSpecialCharacter.search(str(data_attr_summary_headlines[i]['headline']))
+        assert check_for_special_character is None
